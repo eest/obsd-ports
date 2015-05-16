a12f8ea8100660bd081099a16c9a02607576a6b9
Patrik Lundin <patrik@sigterm.se>
2015-05-16 07:44:09 +0200

    Add scripts used when testing the opendnssec port.

---
+++ scripts/opendnssec/patch-ods.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Perform cleanup if opendnssec is currently installed
+if [ -d /var/opendnssec ]; then
+    pkill ods-
+    sleep 3
+    pkg_delete opendnssec
+    rm -rf /etc/opendnssec
+    rm -rf /var/opendnssec
+    rm -rf /var/run/opendnssec
+    /usr/sbin/userdel _opendnssec
+    /usr/sbin/groupdel _opendnssec
+fi
+
+# Remove everything from the build
+cd /usr/ports/security/opendnssec
+make clean=all
+
+echo
+echo "=== to extract the source:"
+echo "cd /usr/ports/security/opendnssec"
+echo "make extract"
+
+echo
+echo "=== to modify a file:"
+echo "cd \`make show=WRKSRC\`"
+echo "cp file-to-edit.c file-to-edit.c.orig"
+echo "<edit file-to-edit.c>"
+echo
+echo "=== to generate a patch from the change:"
+echo "cd /usr/ports/security/opendnssec"
+echo "make update-patches"
+echo "<press enter at prompt and quit the edior that opens>"
+echo "<a patch now exists in the patches/ directory>"
+echo
+echo "=== to clean everything and build/install the package again (will be used by ods-reset.sh):"
+echo "make clean=all"
+echo "make install"
+echo
---
+++ scripts/opendnssec/reset-ods.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Perform cleanup if opendnssec is currently installed
+if [ -d /var/opendnssec ]; then
+    pkill ods-
+    sleep 3
+    pkg_delete opendnssec
+    rm -rf /etc/opendnssec
+    rm -rf /var/opendnssec
+    rm -rf /var/run/opendnssec
+    /usr/sbin/userdel _opendnssec
+    /usr/sbin/groupdel _opendnssec
+fi
+
+# Install the package
+cd /usr/ports/security/opendnssec
+make install
+
+#YBS
+cp /root/yuri/conf.xml /etc/opendnssec/
+
+# Initialize softhsm
+softhsm --init-token --slot 0 --label OpenDNSSEC --so-pin 1234 --pin 1234
+chown _opendnssec /var/opendnssec/softhsm/slot0.db
+
+# Bootstrap OpenDNSSEC
+echo y | ods-ksmutil setup
+echo
+/etc/rc.d/opendnssec start
+
+# Add the unsigned zone file
+cp /root/example.com /var/opendnssec/unsigned/
+
+# Crank up the verbosity
+#ods-signer verbosity 6
+##YBS
+ods-signer verbosity 9
+
+# List the currently running processes:
+echo
+ps auxww | grep ods-
+
+# The last steps are left manual
+echo
+echo "=== to crash ods-signerd run:"
+echo "ods-ksmutil zone add --zone example.com --policy default"
+echo "ods-control enforcer notify"
+
+echo
+echo "=== to get a core dump after it has crashed do this:"
+echo "chpass -s /bin/sh _opendnssec"
+echo "su _opendnssec"
+echo "/usr/local/sbin/ods-signerd -c /etc/opendnssec/conf.xml -d"
+echo "ls -l /var/opendnssec/tmp/"
+echo
